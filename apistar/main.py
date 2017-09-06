842c961c549451310d01858289b8f78faa56d89d
Joan Font <joanfont@gmail.com>
2017-09-06 14:23:24 +0200

    load app from env var `APISTAR_APP`

--- apistar/main.py
+++ apistar/main.py
@@ -1,7 +1,7 @@
 """
 The `apistar` command line client.
 """
-import importlib.util
+import importlib
 import os
 import sys
 
@@ -10,7 +10,7 @@ from apistar.frameworks.cli import CliApp
 from apistar.interfaces import App
 
 
-def load_app() -> App:
+def load_app_from_file() -> App:
     sys.path.insert(0, os.getcwd())
     spec = importlib.util.spec_from_file_location("app", "app.py")
     module = importlib.util.module_from_spec(spec)
@@ -25,13 +25,27 @@ def load_app() -> App:
     return app
 
 
+def load_from_module() -> App:
+    apistar_app = os.getenv('APISTAR_APP')
+    module = importlib.import_module(apistar_app)
+    app = getattr(module, 'app', None)
+    if app is None:
+        msg = "The {} module did not contain an 'app' variable.".format(module)
+        raise exceptions.ConfigurationError(msg)
+    if not isinstance(app, App):
+        msg = "The 'app' variable in '{}' module is not an App instance.".format(module)
+        raise exceptions.ConfigurationError(msg)
+    return app
+
 def default_app() -> App:
     return CliApp()
 
-
 def main() -> None:  # pragma: nocover
     if os.path.exists('app.py'):
-        app = load_app()
+        app = load_app_from_file()
+    elif os.getenv('APISTAR_APP'):
+        app = load_from_module()
     else:
         app = default_app()
+
     app.main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,7 @@ import pytest
 
 from apistar import exceptions
 from apistar.interfaces import App
-from apistar.main import default_app, load_app
+from apistar.main import default_app, load_app_from_file, load_from_module
 
 
 def test_default_app():
@@ -13,7 +13,7 @@ def test_default_app():
     assert isinstance(default, App)
 
 
-def test_load_app():
+def test_load_app_from_file():
     with tempfile.TemporaryDirectory() as tempdir:
         os.chdir(tempdir)
         app = default_app()
@@ -22,19 +22,79 @@ def test_load_app():
         assert isinstance(loaded, App)
 
 
-def test_load_missing_app():
+def test_load_missing_app_from_file():
     with tempfile.TemporaryDirectory() as tempdir:
         os.chdir(tempdir)
         with open('app.py', 'w') as app_file:
             app_file.write('')
         with pytest.raises(exceptions.ConfigurationError):
-            load_app()
+            load_app_from_file()
 
 
-def test_load_invalid_app():
+def test_load_invalid_app_from_file():
     with tempfile.TemporaryDirectory() as tempdir:
         os.chdir(tempdir)
         with open('app.py', 'w') as app_file:
             app_file.write('app = 123\n')
         with pytest.raises(exceptions.ConfigurationError):
-            load_app()
+            load_app_from_file()
+
+
+def test_load_app_from_module():
+    with tempfile.TemporaryDirectory() as root_tempdir:
+        os.chdir(root_tempdir)
+        with tempfile.TemporaryDirectory() as module_tempdir:
+            os.chdir(module_tempdir)
+            with open('__init__.py', 'w') as init:
+                init.write('')
+
+            app = default_app()
+            app.main(['new', '.'], standalone_mode=False)
+
+        os.chdir(root_tempdir)
+        os.putenv('APISTAR_APP', '{}.app'.format(module_tempdir.name))
+
+        loaded = load_app_from_module()
+        assert isinstance(loaded_app, App)
+
+
+def test_load_missing_app_from_module():
+  with tempfile.TemporaryDirectory() as root_tempdir:
+        os.chdir(root_tempdir)
+        with tempfile.TemporaryDirectory() as module_tempdir:
+            os.chdir(module_tempdir)
+            with open('__init__.py', 'w') as init:
+                init.write('')
+
+            with open('app.py', 'w') as app_file:
+                app_file.write('')
+
+        os.chdir(root_tempdir)
+        os.putenv('APISTAR_APP', '{}.app'.format(module_tempdir.name))
+
+        loaded = load_app_from_module()
+        assert isinstance(loaded_app, App)
+
+        with pytest.raises(exceptions.ConfigurationError):
+            load_app_from_module()
+
+
+def test_load_invalid_app_from_module():
+  with tempfile.TemporaryDirectory() as root_tempdir:
+        os.chdir(root_tempdir)
+        with tempfile.TemporaryDirectory() as module_tempdir:
+            os.chdir(module_tempdir)
+            with open('__init__.py', 'w') as init:
+                init.write('')
+
+            with open('app.py', 'w') as app_file:
+                app_file.write('app = 123\n')
+
+        os.chdir(root_tempdir)
+        os.putenv('APISTAR_APP', '{}.app'.format(module_tempdir.name))
+
+        loaded = load_app_from_module()
+        assert isinstance(loaded_app, App)
+
+        with pytest.raises(exceptions.ConfigurationError):
+            load_app_from_module()
